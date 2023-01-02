e3f1a39a82135702eb048effca9e8e039302a8a4
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-02 16:03:02 +0100

    develop - Sidebar functionality and User Profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Routes, Route } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ProSidebarProvider } from "react-pro-sidebar";
 
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
+
 import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 function App() {
 	const [theme, colorMode] = useMode();
@@ -12,9 +25,29 @@ function App() {
 			<ThemeProvider theme={theme}>
 				<CssBaseline></CssBaseline>
 				<div className="app">
+					{/* Sidebar */}
+					<ProSidebarProvider>
+						<Sidebar></Sidebar>
+					</ProSidebarProvider>
+
 					<main className="content">
 						{/* TopBar */}
 						<Topbar></Topbar>
+
+						{/* Routes */}
+						<Routes>
+							<Route path="/" element={<Dashboard></Dashboard>}></Route>
+							{/* <Route path="/team" element={<Team></Team>}></Route> */}
+							{/* <Route path="/contacts" element={<Contacts></Contacts>}></Route> */}
+							{/* <Route path="/invoices" element={<Invoices></Invoices>}></Route> */}
+							{/* <Route path="/form" element={<Form></Form>}></Route> */}
+							{/* <Route path="/bar" element={<Bar></Bar>}></Route> */}
+							{/* <Route path="/pie" element={<Pie></Pie>}></Route> */}
+							{/* <Route path="/line" element={<Line></Line>}></Route> */}
+							{/* <Route path="/faq" element={<FAQ></FAQ>}></Route> */}
+							{/* <Route path="/geography" element={<Geography></Geography>}></Route> */}
+							{/* <Route path="/calendar" element={<Calendar></Calendar>}></Route> */}
+						</Routes>
 					</main>
 				</div>
 			</ThemeProvider>
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,7 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+	Sidebar as ProSideBar,
+	Menu,
+	useProSidebar,
+	MenuItem,
+} from "react-pro-sidebar";
+import {
+	Box,
+	IconButton,
+	Typography,
+	useTheme,
+	MenuOutlinedIcon,
+	Icon,
+} from "@mui/material";
+import { MenuOutlined } from "@mui/icons-material";
+import { tokens } from "../../theme";
 
 const Sidebar = () => {
-	return <div>Sidebar</div>;
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+	const { collapseSidebar, collapsed } = useProSidebar();
+	const [selected, setSelected] = useState("Dashboard");
+
+	return (
+		<Box
+			sx={{
+				"& .pro-sidebar-inner": {
+					background: `${colors.primary[400]} !important`,
+				},
+				"& .pro-icon-wrapper": {
+					backgroundColor: "transparent !important",
+				},
+				"& .pro-inner-item": {
+					padding: "5px 35px 5px 20px !important",
+				},
+				"& .pro-inner-item:hover": {
+					color: "#868dfb !important",
+				},
+				"& .pro-inner-item.active": {
+					color: "#6870fa !important",
+				},
+			}}
+		>
+			<ProSideBar
+				defaultCollapsed={true}
+				style={{ height: "100%" }}
+				backgroundColor={`${colors.primary[400]} !important`}
+			>
+				<Menu>
+					{/* Logo & Menu Icon */}
+					<MenuItem
+						onClick={() => collapseSidebar()}
+						icon={collapsed ? <MenuOutlined></MenuOutlined> : undefined}
+						style={{
+							margin: "10px 0 20px 0",
+							color: colors.grey[100],
+						}}
+					>
+						{!collapsed && (
+							<Box
+								display="flex"
+								justifyContent="space-between"
+								alignItems="center"
+								ml="15px"
+							>
+								<Typography variant="h3" color={colors.grey[100]}>
+									ADMINIS
+								</Typography>
+								<IconButton>
+									<MenuOutlined></MenuOutlined>
+								</IconButton>
+							</Box>
+						)}
+					</MenuItem>
+
+					{/* User Profile */}
+
+					{!collapsed && (
+						<Box mb="25px">
+							<Box display="flex" justifyContent="center" alignItems="center">
+								<img
+									src={`../../assets/user.png`}
+									alt="profile-user"
+									width="100px"
+									height="100px"
+									style={{ cursor: "pointer", borderRadius: "50%" }}
+								/>
+							</Box>
+							<Box textAlign="center">
+								<Typography
+									variant="h2"
+									color={colors.grey[100]}
+									fontWeight="bold"
+									sx={{
+										m: "10px 0 0 0",
+									}}
+								>
+									Ed Roh
+								</Typography>
+								<Typography variant="h5" color={colors.greenAccent[500]}>
+									VP Fancy Admin
+								</Typography>
+							</Box>
+						</Box>
+					)}
+				</Menu>
+			</ProSideBar>
+		</Box>
+	);
 };
 
 export default Sidebar;
